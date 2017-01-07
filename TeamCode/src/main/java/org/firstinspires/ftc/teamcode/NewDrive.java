9e1b4fe4c234f7cd39f4d220bc40e23fe7b64199
Josh Bruinsma <joshdbruinsma@gmail.com>
2017-01-07 17:05:57 -0600

    Meaningless change to demo Git

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewDrive.java
@@ -10,18 +10,45 @@ import static com.qualcomm.ftccommon.R.layout.servo;
  */
 
 public class NewDrive extends OpMode {
-    DcMotor FrontWheel;
-    DcMotor BackLeftWheel;
-    DcMotor BackRightWheel;
+    DcMotor W0;
+    DcMotor W1;
+    DcMotor W2;
 
     @Override
     public void init() {
-        FrontWheel = hardwareMap.dcMotor.get("Front_Wheel");
-        BackLeftWheel = hardwareMap.dcMotor.get("Back_Left_Wheel");
-        BackRightWheel = hardwareMap.dcMotor.get("Back_Right_Wheel");
+        W0 = hardwareMap.dcMotor.get("W0");
+        W1 = hardwareMap.dcMotor.get("W1");
+        W2 = hardwareMap.dcMotor.get("W2");
     }
     @Override
     public void loop(){
+        double x = gamepad1.left_stick_x;
+        double y = -gamepad1.left_stick_y;
+        double angle = 90;
+        if(x==0){
+            if(y>0){
+                angle=90;
+            } else if (y<0){
+                angle=270;
+            }
+        } else {
+            angle=Math.atan(y/x)*180/Math.PI;
+        }
+        if(angle<=180&&angle>=0){
+            W0.setPower((90-angle)/90);
+        }else {
+            W0.setPower((angle-270)/90);
+        }
+        //power for wheel 0
+
+
+        if(angle>=120&&angle<=300){
+            W1.setPower((210-angle)/90);
+        }else{
+            W1.setPower(())
+        }
+
+
 
 
     }
